8b0a632d046e5959e9cd320a151def50b1f16343
alanskverer <59963384+alanskverer@users.noreply.github.com>
2021-02-19 19:10:10 +0200

    Choose picture from gallery done

---
+++ client/src/components/MyOverlay.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Button, Overlay } from 'react-native-elements';
+
+const MyOverlay = (props) => {
+    return (
+        <Overlay isVisible={props.isVisible} onBackdropPress={props.onBackdropPress} overlayStyle={styles.overlay}>
+            {props.children}
+        </Overlay>
+    )
+}
+
+export default MyOverlay
+
+const styles = StyleSheet.create({
+    overlay: {
+        height: '35%',
+        width: '80%'
+
+    },
+
+})
+
