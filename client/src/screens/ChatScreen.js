a0124760a400e85fa16dffafa08863336a621a47
Offek Kes <offek.kessel@ruppin365.net>
2021-03-25 14:04:58 +0200

    More chat support

--- client/src/screens/ChatScreen.js
+++ client/src/screens/ChatScreen.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux';
+import MyLinearGradient from '../components/MyLinearGradient';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import { KeyboardAvoidingView } from 'react-native';
+import MyOverlay from '../components/MyOverlay';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import FontAwsome5 from 'react-native-vector-icons/FontAwesome5';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { windowHeight, windowWidth } from '../../utils/Dimentions';
+import axios from 'axios';
+import Post from '../components/Post';
+import ChatContact from '../components/ChatContact';
 
 const ChatScreen = () => {
     return (
-        <View style={styles.container}>
-            <Text>ChatScreen </Text>
-        </View>
+        // <View style={styles.container}>
+        //     <Text>ChatScreen </Text>
+        // </View>
+        <KeyboardAvoidingView style={styles.container} >
+            <View style={styles.inner}>
+                <MyLinearGradient firstColor="#00c6fb" secondColor="#005bea" height={90} />
+                {/* <MyLinearGradient firstColor="#f5f7fa" secondColor="#c3cfe2" height={80} /> */}
+
+
+                <View style={styles.barContainer}><Text style={styles.barText}>Chat</Text>
+                    
+                </View>
+            </View>
+            <ScrollView>
+                <ChatContact />
+                
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -14,7 +41,71 @@ export default ChatScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: '#fff',
+        // justifyContent: 'center',
+        // alignItems: 'center'
+    },
+    inner: {
+        padding: windowHeight / 45,
+
+        flex: 1,
+        //  justifyContent: "space-around"
+    },
+    categoryContainer: {
+        flex: 1,
+        marginTop: 30,
+        marginBottom: 60,
+
+    },
+
+    barContainer: {
+        // flex: 1,
+
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        //  marginLeft: 30,
+        marginTop: windowHeight / 40,
+        flexDirection: 'row',
+        paddingLeft: windowWidth / 100,
+        paddingRight: windowWidth / 100,
+
+    },
+    barText: {
+        color: "#ffffff",
+        fontSize: 24,
+        fontWeight: 'bold',
+
+    },
+    bellIcon: {
+        color: '#ffffff',
+        fontSize: 24
+    },
+    selectCategoryContainer:
+    {
+        flex: 1,
+        marginTop: 30,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-around',
+        marginBottom: 60,
+        width: '100%',
+        // marginLeft: windowWidth / 50,
+        borderRadius: 50
+    }, dropDownContainer: {
+
+        width: '85%',
+        height: windowHeight / 15,
+
+    },
+    filterICon: { marginLeft: 30, marginTop: 15 },
+
+    postsContainer: {
+        //    / flex: 1,
+        //   minHeight: 140
+        // justifyContent: '',
+        //alignItems: 'center'
+    },
+    userGreetingText: {
+        fontSize: 18
     }
 })
