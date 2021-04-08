12630f6b94c7fb6e886c18767a5ae74d98cd829b
Offek Kes <offek.kessel@ruppin365.net>
2021-04-08 12:18:41 +0300

    Location error handling

--- WebApi/Controllers/PostController.cs
+++ WebApi/Controllers/PostController.cs
@@ -631,9 +631,9 @@ namespace WebApi.Controllers
                         text = y.text
                     }).ToList(),
 
-                    distanceFromMe = (double)(12742 * System.Data.Entity.SqlServer.SqlFunctions.Asin(System.Data.Entity.SqlServer.SqlFunctions.SquareRoot((double)(0.5 - System.Data.Entity.SqlServer.SqlFunctions.Cos((filterDTO.meetingLocationLat - x.latitude) * 0.017453292519943295) / 2 +
+                    distanceFromMe = (filterDTO != null)?(double)(12742 * System.Data.Entity.SqlServer.SqlFunctions.Asin(System.Data.Entity.SqlServer.SqlFunctions.SquareRoot((double)(0.5 - System.Data.Entity.SqlServer.SqlFunctions.Cos((filterDTO.meetingLocationLat - x.latitude) * 0.017453292519943295) / 2 +
           System.Data.Entity.SqlServer.SqlFunctions.Cos(x.latitude * 0.017453292519943295) * System.Data.Entity.SqlServer.SqlFunctions.Cos(filterDTO.meetingLocationLat * 0.017453292519943295) *
-          (1 - System.Data.Entity.SqlServer.SqlFunctions.Cos((filterDTO.meetingLocationLong - x.longitude) * 0.017453292519943295)) / 2    )))) // 2 * R; R = 6371 km
+          (1 - System.Data.Entity.SqlServer.SqlFunctions.Cos((filterDTO.meetingLocationLong - x.longitude) * 0.017453292519943295)) / 2    )))):-999 // 2 * R; R = 6371 km
                 //distanceFromMe = (double)System.Data.Entity.SqlServer.SqlFunctions.SquareRoot((double)9)
                 //distanceFromMe = (double)((x.latitude - filterDTO.meetingLocationLat) * (x.latitude - filterDTO.meetingLocationLat) + (x.longitude - filterDTO.meetingLocationLong) * (x.longitude - filterDTO.meetingLocationLong))
                 /*Coordinates = db.Posts.Where(z => z.id == x.id).Select(z => new GeoCoordinate(){
